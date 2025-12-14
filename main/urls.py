1ef411bb7f76328433cfa9f79cc0efe79dc1edc3
Nathan_ <hamidoulila14@gmail.com>
2025-12-14 05:14:21 +0100

    cleaned the view and urls, made the front pages button logic and some minor style

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# sets the list of urls the user will visit
 urlpatterns = [
     path('', views.home, name='home'),
+    path('character/', views.character),
 ]
--- main/views.py
+++ main/views.py
@@ -2,16 +2,15 @@ from django.shortcuts import render
 from .models import FrameData, Characters
 
 def parse_move(move_str):
-    """
-    Parse Tekken move strings into a list of display elements.
-    Handles:
-    - ',' separates moves
-    - '~' wraps in brackets
-    - '/' merges directional inputs
-    - '+' splits directional inputs from numbers if needed
-    - Text labels (WS, FC, SS) stay as text
-    - Multi-button combos stay combined
-    """
+    # Parse Tekken move strings into a list of display elements.
+    #       Handles:
+    # - ',' separates moves
+    # - '~' wraps in brackets
+    # - '/' merges directional inputs
+    # - '+' splits directional inputs from numbers if needed
+    # - Text labels (WS, FC, SS) stay as text
+    # - Multi-button combos stay combined
+
     TEXT_LABELS = {"WS", "FC", "SS"}
     result = []
 
@@ -67,7 +66,20 @@ def parse_move(move_str):
 
     return result
 
+#request for the home page, sends all the character names to make the buttons for the user
 def home(request):
+    character_list = Characters.objects.all()
+    characters = Characters.objects.all()
+
+    character_list = []
+    for character in characters:
+        character_list.append((character.name))
+
+    return render(request, "home.html", {"character_list" : character_list})
+
+#request for the character page, pulls the full movelist of a character based on the buttons pressed
+#then uses the parsing function to make it understandable to a human being.
+def character(request):
     character = request.GET.get("character")
     character_list = Characters.objects.all()
     framedata = FrameData.objects.filter(character__name=character)
@@ -76,4 +88,6 @@ def home(request):
     for move in framedata:
         moves_list.append(parse_move(move.move))  # append list of keys
 
-    return render(request, "index.html", {"moves_list": moves_list, "characters" : character_list})
+    return render(request, "character.html", {"moves_list": moves_list, "character_list" : character_list, "character_name" : (character).replace("_", " ")})
+
+    
