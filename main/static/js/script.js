3b1bbb1c53f0da2c15fb3b393918274a9f754ff9
Nathan_ <hamidoulila14@gmail.com>
2026-03-19 02:23:53 +0100

    made the JS and CSS for the glossary button

---
+++ main/static/js/script.js
@@ -0,0 +1,71 @@
+ var IsGlossaryOpen = false;
+
+window.onload = () => {
+    const glossary_btn = document.querySelector(".glossary_btn");
+    const glossary_div = document.querySelector("#glossary_div");
+    const glossary_div_container = document.querySelector("#glossary_div_container");
+            
+    glossary_btn.addEventListener("click", function(){
+
+    ToggleGlossary();        
+   })
+}
+
+ToggleGlossary = function(){
+    if(IsGlossaryOpen == false){
+        console.log("oui je suis fermé 0");
+        IsGlossaryOpen = true;
+        glossary_div.style.display='flex';
+        glossary_div_container.style.display='flex';
+        disableScroll();
+    }else{
+        console.log("oui je suis ouvert 1");
+        IsGlossaryOpen = false;
+        glossary_div.style.display='none';
+        glossary_div_container.style.display='none';
+        enableScroll();
+    }
+    console.log(IsGlossaryOpen);
+}
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  document.body.style.overflow = 'hidden';;
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  document.body.style.overflow = 'visible';;
+
+}
