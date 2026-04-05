a0fd44fc63ffaa961d7f5d5ec6bcc67b024562cc
Nathan_ <hamidoulila14@gmail.com>
2026-04-06 00:05:51 +0200

    made the javascript logic for the search url

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -1,33 +1,46 @@
  var IsGlossaryOpen = false;
 
 window.onload = () => {
-    const glossary_btn = document.querySelector(".glossary_btn");
-    const glossary_div = document.querySelector("#glossary_div");
-    const glossary_div_container = document.querySelector("#glossary_div_container");
-    const search_button = document.querySelector(".search_button")
-            
-    glossary_btn.addEventListener("click", function(){
+  const glossary_btn = document.querySelector(".glossary_btn");
+  const glossary_div = document.querySelector("#glossary_div");
+  const glossary_div_container = document.querySelector("#glossary_div_container");
+  const search_bar = document.querySelector("#move_search");
+  var character_name = search_bar.dataset.charactername
 
-    ToggleGlossary();        
-   })
+  glossary_btn.addEventListener("click", function(){
+
+  ToggleGlossary();        
+  })  
+
+    search_bar.addEventListener("keyup", function(){
+      if(search_bar.value != ""){
+      window.history.pushState(null, document.title, "?character=" + character_name + "?search=" + search_bar.value)
+      }else{
+      window.history.pushState(null, document.title, "?character=" + character_name)
+    }
+  })
 }
 
 ToggleGlossary = function(){
     if(IsGlossaryOpen == false){
-        console.log("oui je suis fermé 0");
         IsGlossaryOpen = true;
         glossary_div.style.display='flex';
         glossary_div_container.style.display='flex';
         disableScroll();
     }else{
-        console.log("oui je suis ouvert 1");
         IsGlossaryOpen = false;
         glossary_div.style.display='none';
         glossary_div_container.style.display='none';
         enableScroll();
     }
     console.log(IsGlossaryOpen);
+
+    search_button.addEventListener("click", function(){
+    search();        
+   })
 }
+
+
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
@@ -69,11 +82,4 @@ function enableScroll() {
   window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
   document.body.style.overflow = 'visible';;
 
-}
-search_button.addEventListener("click", function(){
-    search();        
-   })
-
-function search() {
-  location.href = 'http://127.0.0.1:8000/character/?character=Dragunov&search=' + document.getElementById('move_search')
 }
