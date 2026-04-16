85371bd2a8a963bb4d49b2b89f7649852f1b69e8
TeeGOD <vrcprotom@gmail.com>
2026-04-16 23:09:39 +0200

    perhaps ?

--- tagframe/settings.py
+++ tagframe/settings.py
@@ -132,6 +132,7 @@ INTERNAL_IPS = ["127.0.0.1"]
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "main" / "static",
 ]
